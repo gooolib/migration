2f072cfcdaaeabb3244269ce3664fc3f422d2c79
Jiro <j.code.public@gmail.com>
2025-08-30 16:06:11 +0900

    fix for ci

--- command/status.go
+++ command/status.go
@@ -26,14 +26,16 @@ func (c *StatusCommand) Exec() error {
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 
-	fmt.Fprintln(w, "\n")
+	fmt.Fprintln(w, "")
+	fmt.Fprintln(w, "")
 	fmt.Fprintf(w, "|\tVersion\t|\tStatus\t|\n")
 	fmt.Fprintln(w, "+\t=================\t+\t========\t+")
 	for _, status := range statuses {
 		fmt.Fprintln(w, fmt.Sprintf("|\t%s\t|\t%s\t|", status.Version, status.Status))
 	}
 	fmt.Fprintln(w, "+\t=================\t+\t========\t+")
-	fmt.Fprintln(w, "\n")
+	fmt.Fprintln(w, "")
+	fmt.Fprintln(w, "")
 	w.Flush()
 
 	return nil
